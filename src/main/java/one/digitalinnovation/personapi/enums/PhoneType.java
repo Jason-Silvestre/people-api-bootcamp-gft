697a0fd2fe67090b6718ce5bc981e8a207489719
JasonSilvestre <jasonsilvestre43@yahoo.com>
2021-07-29 11:40:37 -0300

    alterando enum PhoneType commit

---
+++ src/main/java/one/digitalinnovation/personapi/enums/PhoneType.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.personapi.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum PhoneType {
+
+//        HOME(description:"Home"),
+//
+//        MOBILE(description:"Mobile"),
+//
+//        COMMERCIAL(description:"Comercial");
+//
+//        private final String description;
+
+    }
+
